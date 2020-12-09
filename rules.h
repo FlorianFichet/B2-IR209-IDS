89a5d7de1e27c90d58147b65b052d60ffca57b59
Grégoire Roumache <groumache@gmail.com>
2020-12-09 15:28:16 +0100

    add enum 'rule_protocol' that enumerates every possible protocol a rule can user

--- rules.h
+++ rules.h
@@ -9,10 +9,40 @@ enum rule_action {
     rejectdst,
     rejectboth,
 } typedef RuleAction;
+enum rule_protocol {
+    tcp,
+    udp,
+    icmp,
+    ip,
+    http,
+    ftp,
+    tls, // (this includes ssl)
+    smb,
+    dns,
+    dcerpc,
+    ssh,
+    smtp,
+    imap,
+    modbus,
+    dnp3,
+    enip,
+    nfs,
+    ikev2,
+    krb5,
+    ntp,
+    dhcp,
+    rfb,
+    rdp,
+    snmp,
+    tftp,
+    sip,
+    http2,
+} typedef RuleProtocol;
 
 
 struct ids_rule {
     RuleAction action;
+    RuleProtocol protocol;
 } typedef Rule;
 
 
